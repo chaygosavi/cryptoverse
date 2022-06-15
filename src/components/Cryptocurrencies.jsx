80801119a2050810e43373156838e974495b68ae
Chaitanya Gosavi <chaitanyagosavi540@gmail.com>
2022-06-15 17:36:40 +0530

    first commit

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Cryptocurrencies = ({ simpified }) => {
+  const count = simpified ? 10 : 100;
+  const { data, isFetching } = useGetCryptosQuery(count);
+
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filterData = data?.data?.coins?.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filterData);
+  }, [searchTerm, data]);
+
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      {!simpified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            key={currency.uuid}
+            className="crypto-card"
+          >
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cao: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Cryptocurrencies;
---
+++ src/components/News.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImgUrl =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+const News = ({ simplified }) => {
+  const { data } = useGetCryptosQuery(100);
+
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrrency");
+
+  const { data: cryptNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 10,
+  });
+
+  if (!cryptNews?.value) return "Loading...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crpto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+          >
+            <Option value={"Cryptocurrrency"}>Cryptocurrrency</Option>
+            {data?.data?.coins?.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptNews?.value?.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news_title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImgUrl}
+                  alt="news"
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImgUrl
+                    }
+                    alt=""
+                  />
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default News;
